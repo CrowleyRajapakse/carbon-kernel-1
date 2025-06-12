38e4bd434c398be7d757d7aa5fe16fe779a5737a
sgayangi <gayangi.18@cse.mrt.ac.lk>
2025-06-12 14:33:23 +0530

    Update implementation to use secure transformer

--- core/org.wso2.carbon.tomcat/src/main/java/org/wso2/carbon/tomcat/internal/ServerManager.java
+++ core/org.wso2.carbon.tomcat/src/main/java/org/wso2/carbon/tomcat/internal/ServerManager.java
@@ -55,7 +55,6 @@ import javax.xml.transform.Result;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
